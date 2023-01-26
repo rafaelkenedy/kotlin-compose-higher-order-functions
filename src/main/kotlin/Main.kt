0b7dcf073d4f38e348716eb081712c77c54009b1
rafael <k.rafaelalves@gmail.com>
2023-01-26 17:47:10 -0300

    ok

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -52,9 +52,13 @@ val cookies = listOf(
 
 fun main() {
 
-    cookies.forEach {
-        println("Menu item: ${it.name}")
+    val fullMenu = cookies.map {
+        "${it.name} - $${it.price}"
     }
 
+    println("Full menu: ")
+    fullMenu.forEach{
+        println(it)
+    }
 
 }
